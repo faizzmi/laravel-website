d0406aba22015eecb0969d7b5c1dc7d97e5814ac
Faiz7401 <83203236+Faiz7401@users.noreply.github.com>
2024-02-03 22:39:05 +0800

    first honme page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1>Welcome to my dashboard</h1>
+        <p>Welcome to my portfolio! As a Junior Software Engineer, I showcase my coding, design, and collaboration skills.</p> 
+        <p>Explore projects, view my resume, and let's connect for exciting opportunities! </p>
+    </center>
+</body>
+</html>
